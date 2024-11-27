7cc8b4a051bb978fad41fa65a503fa4f006c4d87
Romanoz87 <chkaduaroman@gmail.com>
2024-11-27 19:33:26 +0400

    update

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,7 +1,11 @@
 
 from guessed_word import my_func  # ვაიმპორტირებთ ჩაფიქრებულ რანდომ სიტყვას 
 
+
+
 print('\nGuess fruit in 10 try')
+
+
 selected_item = my_func()
 list1 = list(selected_item)
 
@@ -13,32 +17,40 @@ for _ in range(len(selected_item)):
     list_of.append("*")
 print('\nGuess random word:_', *list_of)
 print("\n")
+
+match = 0
 i = 1
 while i <= 10:
-    user_input = input("\ninput word or symbol to guess: ")
+    user_input = input("\ninput word or symbol to guess:_ ")
 
     if i < 10:
-        match = 0
+        
         if user_input in list1:
-
+            
             indexes = [index for index, value in enumerate(list1) if value == user_input]
             for index in indexes:
             
                 list_of[index] = list1[index]
                 
-            print("you guessed one symbol in word.\n")
-            print(*list_of)
-            match += 1
-            
+                print("\nyou guessed one symbol in word.\n")
+                print(*list_of)
+                match += 1
             if match == len(selected_item):
-                    print("congrats:..")
+                print("\n....Congrats:.. you guessed the word.....")   
+                break
+                
+        else:
+                    print("wrong symbol")
+                
+        
+    
+        
             
                     
-        else:
-                print("wrong symbol")
+        
     i += 1
 else:
     
     print("Sorry you didnt guess the word")
     
-print("end of game")
+print("............end of game............")
