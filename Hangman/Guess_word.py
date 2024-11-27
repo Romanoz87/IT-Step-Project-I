ce811030f4aaebb8a5765b8227f1c8d724ade071
Romanoz87 <chkaduaroman@gmail.com>
2024-11-27 16:15:13 +0400

    update

---
+++ Hangman/Guess_word.py
@@ -0,0 +1,43 @@
+
+from guessed_word import my_func
+
+selected_item = my_func()
+list1 = list(selected_item)
+# print(list1)
+list_of = [] # ცარიელი სიმბოლოების ლისტი. 
+
+
+for _ in range(len(selected_item)):
+    
+    list_of.append("*")
+print('\nGuess random word:_', *list_of)
+print("\n")
+i = 1
+while i <= 10:
+    user_input = input("\ninput word or symbol to guess: ")
+
+    if i < 10:
+        match = 0
+        if user_input in list1:
+
+            indexes = [index for index, value in enumerate(list1) if value == user_input]
+            for index in indexes:
+            
+                list_of[index] = list1[index]
+                
+            print("you guessed one symbol in word.\n")
+            print(*list_of)
+            match += 1
+            
+            if match == len(selected_item):
+                    print("congrats:..")
+            
+                    
+        else:
+                print("wrong symbol")
+    i += 1
+else:
+    
+    print("Sorry you didnt guess the word")
+    
+print("end of game")
---
+++ Hangman/guessed_word.py
@@ -0,0 +1,8 @@
+import random
+
+def my_func():
+# List of items
+    items = ['apple', 'banana', 'cherry', 'date', 'elder']
+
+    # Select a random item
+    return random.choice(items)
