d608703571681d7c2c3ffb0cc3350e0bb94fb1ca
Romanoz87 <chkaduaroman@gmail.com>
2024-11-27 20:37:22 +0400

    update

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,14 +1,9 @@
 
-from guessed_word import my_func  # ვაიმპორტირებთ ჩაფიქრებულ რანდომ სიტყვას 
-
-
-
-print('\nGuess fruit in 10 try')
-
+from guessed_word import my_func
 
 selected_item = my_func()
 list1 = list(selected_item)
-
+# print(list1)
 list_of = [] # ცარიელი სიმბოლოების ლისტი. 
 
 
@@ -17,40 +12,33 @@ for _ in range(len(selected_item)):
     list_of.append("*")
 print('\nGuess random word:_', *list_of)
 print("\n")
-
-match = 0
 i = 1
 while i <= 10:
-    user_input = input("\ninput word or symbol to guess:_ ")
+    user_input = input("\ninput word or symbol to guess: ")
 
     if i < 10:
         
         if user_input in list1:
-            
+
             indexes = [index for index, value in enumerate(list1) if value == user_input]
             for index in indexes:
             
                 list_of[index] = list1[index]
-                match += 1
-            
+                
+            print("you guessed one symbol in word.\n")
             print(*list_of)
-               
-            if match == len(selected_item):
-                print("\n....Congrats:.. you guessed the word.....")   
+            
+            
+            if list_of == list1:
+                print("\n...congrats you guessed word..\n")
                 break
-                
-        else:
-                    print("wrong symbol")
-                
-        
-    
-        
             
                     
-        
+        else:
+                print("wrong symbol")
     i += 1
 else:
     
-    print("Sorry you didnt guess the word")
+    print("\nSorry you didnt guess the word\n")
     
-print("............end of game............")
+print("...Game Over...")
