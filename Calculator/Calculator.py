ede4a62f773bc5c8af56ee12318978e9f9be2e14
Romanoz87 <chkaduaroman@gmail.com>
2024-11-27 18:36:06 +0400

    update

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,43 +1,49 @@
 #Calculator 
+import sys
 
 print("Calculator app")
-
-try:
-    number1 = eval(input("Enter first number: "))
-    number2 = eval(input("Enter second number: "))
-
-
-    symbol = input("Enter Mathematical operation (+, -, *, /, or %): ")
-    allowed_symbols = ['+', '-', '*', '/', '%']
-
-
-    if symbol in allowed_symbols:
-
-        if symbol == '+':
-            print(f"{number1} + {number2} = {number1 + number2}")
-
-        elif symbol == '-':
-            print(f"{number1} - {number2} = {number1 - number2}")
-
-        elif symbol == '*':
-            print(f"{number1} * {number2} = {number1 * number2}")
-
-        elif symbol == '/':
-            if number1 == 0 or number2 == 0:
-                raise ZeroDivisionError("You cannot divide by zero.")
-            print(f"{number1} / {number2} = {number1 / number2}")
-
-        elif symbol == '%':
-            number3 = number2/100*number1
-            print(f"{number1} % {number2} = ",  number3)
+while True:
+    allowed_symbols = ['+', '-', '*', '/', '%', 'x']
+    print("-"*100)
+    symbol = input("Enter Mathematical operation (+, -, *, /, % or x): ").lower()
+    if symbol in allowed_symbols and symbol == 'x':
+        print("exit calculator")
+        sys.exit(1)
+    else:        
+        try:
+            if symbol in allowed_symbols:
+                
+                number1 = eval(input("Enter first number: "))
+                number2 = eval(input("Enter second number: "))
 
             
-        else:
-            raise ValueError("Invalid operator. Please use +, -, *, /, or %.")
-        
+
+                if symbol == '+':
+                    print(f"{number1} + {number2} = {number1 + number2}")
+
+                elif symbol == '-':
+                    print(f"{number1} - {number2} = {number1 - number2}")
+
+                elif symbol == '*':
+                    print(f"{number1} * {number2} = {number1 * number2}")
+
+                elif symbol == '/':
+                    if number1 == 0 or number2 == 0:
+                        raise ZeroDivisionError("You cannot divide by zero.")
+                    print(f"{number1} / {number2} = {number1 / number2}")
+
+                elif symbol == '%':
+                    number3 = number2/100*number1
+                    print(f"{number1} % {number2} = ",  number3)
+
+               
+                    
+            else:
+                print("Invalid operator. Please use +, -, *, /, %, or x.")
+                
 
 
-except NameError as er:
-    print("Enter only numbers")
-except SyntaxError as sy:
-    print("Not alowed mathematical action with symbols")
+        except NameError as er:
+            print("Enter only numbers")
+        except SyntaxError as sy:
+            print("Not alowed mathematical action with symbols")
