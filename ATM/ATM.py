37d5238a642259e516acf5cd8888c4fa2d97a836
Romanoz87 <chkaduaroman@gmail.com>
2024-12-02 15:37:11 +0400

    update

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -9,7 +9,7 @@ from json import JSONEncoder
     # (111111111, 1111, 9000),
     # (112211111, 1311, 9000),   
 
-
+#==================================================================================================
 #დეკორატორი პინ კოდით ვალიდაციისთვის
 def verification(func):                             
         def wrapper(*args, **kwargs):
@@ -23,27 +23,29 @@ def verification(func):
                     print(f'\nInput correct pin number\n')
                              
         return wrapper
-
-     
-
+    
+#==================================================================================================
 # ფაილიდან ამოსაკითხი ფუნქცია
 def read_data():
     with open('atm.json', mode='r', encoding='utf-8') as file:
         return json.load(file)
+    
 # ფაილში შენახვის ფუნქცია
 def write_data(data):
     
     with open("atm.json", mode = 'w', encoding='utf-8') as file:
         json.dump(data, file, cls=jsonencode, indent=2)
 
-# ძირითადი კლასი მონაცემთა ბაზის შესაქმნელად
+
+#==================================================================================================
+# ძირითადი კლასი 
 class Atm:
     def __init__(self, card_id=None, pin=None, ballance=None):
         
         self.__card_id = card_id
         self.__pin = pin
         self.ballance = ballance
-   
+#==================================================================================================   
 #ფუნქცია თანხის გამოსატანად    
     def withdrow_cash(self, withdraw=0, fee=0.01):
         
@@ -59,9 +61,7 @@ class Atm:
                     print("In process...")
                     bal['ballance'] = bal['ballance'] - (withdraw + (withdraw * fee))
                     return result
-        
-    
-
+#==================================================================================================
 # ფუნქცია ბალანსის შესავსებად  
     def add_ballance(self, input_amount=0):
         
@@ -70,8 +70,7 @@ class Atm:
             if input_card == bal['_Atm__card_id']:
                 bal['ballance'] = bal['ballance']+ input_amount
         
-
-
+#==================================================================================================
 # ფუნქცია ბალანსის სანახავად    
     def see_ballance(self):
         for bal in result:
@@ -80,6 +79,7 @@ class Atm:
                 print("-"*50)
                 print(f"Your remaining ballance is {bal['ballance']}$\n\n")
                 print("-"*50)
+#==================================================================================================
 # ფუნქცია პინის შესაცვლელად    
     def change_pin(self, new_pin):
         self.new_pin = new_pin
@@ -87,15 +87,13 @@ class Atm:
             if input_card == pin['_Atm__card_id']:
                 pin['_Atm__pin'] = self.new_pin
 
-
-
-
+#  ენკოდერი
 class jsonencode(JSONEncoder):
     def default(self, o):
         return o.__dict__
 
 
-# მომხმარებელთა მონაცებემი, ბარათის ნომერი, პინი და ბალანსი.
+# მომხმარებელთა მონაცებემი, რომელიც გადაეცემა ძირითად კლასს ბარათის ნომერი, პინი და ბალანსი.
 customers_data = [
     Atm(123456789, 1234, 5000),
     Atm(123456783, 2345, 6000),
@@ -105,7 +103,7 @@ customers_data = [
     Atm(112211111, 1311, 9000),   
 ]
 
-
+#---------------------------------------------------------------------------------------
 json_filename = 'ATM' + '.json'
     
 # შემოწმება თუ უკვე არსებობს ფაილი მეორედ არ ჩაიწერება
@@ -124,7 +122,7 @@ else:
 
 result = read_data()  # ვიძახებთ ფაილიდან წაკითხვის ფუნქციას
 
-
+#==================================================================================
 # ფუნქცია მენიუს გამოსატანად და მოქმედებების განსახორციელებლად
 
 print("\nWelcome..\n")
@@ -137,21 +135,20 @@ def action():
         print("Type 'b' to see your ballance\nType 'w' to withdrow cash\nType 'a' to add amount to your ballance\nType 'p' to change pin code\nType 'x' to exit")
 
         action = input('\nEnter symbol___ ').lower()
-
+#_________________________________________________________________________________________________________________
 # 'x' შეყვანის შემთხვევაში მომხმარებელი გადის ბანკომატის პროგრამიდან და ინახავს საბოლოო მონაცემს        
         if action == 'x':                               
             print('Exit...')
             write_data(result)
             
             break
-
+#_________________________________________________________________________________________________________________
 # 'w' შეყვანს შემთხვევაში მომხმარებელი იძახებს თანხის გამოტანის ფუნქციას             
         elif action == 'w':                                              
             withdraw = eval(input("Enter withdraw amount: "))
             customer1.withdrow_cash(withdraw)
-
+#_________________________________________________________________________________________________________________
 # 'a' შეყვანის შემთხვევაში მომხმარებელი იძახებს თანხის დამატების ფუნქციას
-
         elif action == 'a':
             try:
                 input_amount = eval(input("Please put cash in ATM: "))
@@ -159,12 +156,12 @@ def action():
                 print('enter only numbers')
             customer2 = Atm()
             customer2.add_ballance(input_amount)
-
+#_________________________________________________________________________________________________________________
 # 'b' შეყვანის შემთხვევაში მომხმარებელი ნახულობს დარჩენილ ბალანსს
         elif action == 'b':
             customer1 = Atm()
             customer1.see_ballance()
-
+#_________________________________________________________________________________________________________________
 # 'p' შეყვანის შემთხვევაში  ვიძახებთ პინის შეცვლის ფუნქციას       
         elif action == 'p':
             
@@ -186,7 +183,8 @@ def action():
         else:
             print("\nplease enter only valid symbols ( a, b, w, p or x)\n")
 
-        
+#==================================================================================================================================
+# მომხმარებელს კონსოლიდან შეჰყავს id ბარათის მონაცემები და პინ კოდი გამოირიცხება არასწორი სიმბოლოების შეყვანა
 print("Enter Id and pin numbers below \n")
 
 try:
